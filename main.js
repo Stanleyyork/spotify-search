5cc69531c2c67051ddd00d8044b2ef4f6c4dac5d
stanleyyork <stanleyyork@gmail.com>
2015-11-03 18:52:16 -0800

    first commit; includes fulfillment of basic requirements

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ $(function() {
   // check to make sure JS is loaded
   console.log('JS is loaded!');
   
-  // your code here
+	$('#search').on('submit', function(event){
+		event.preventDefault();
+		$('#results').text(" ");
+		var query = $('#search_query').val();
+		$.get('https://api.spotify.com/v1/search?q='+query+'&type=track', function(data){
+			var results = data.tracks.items;
+			console.log(results);
+			results.forEach(function(r){
+				$('#results').append("<li>" + r.artists[0].name + ": " + r.name + "</li>");
+			});
+
+		});
+
+	});
 
 });
