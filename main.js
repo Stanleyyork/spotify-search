7461ddb22972171b4bde1d8c5ef5fb022639b2a3
stanleyyork <stanleyyork@gmail.com>
2015-11-03 22:42:00 -0800

    last commit

--- main.js
+++ main.js
@@ -6,19 +6,25 @@ $(function() {
   
 	$('#search').on('submit', function(event){
 		event.preventDefault();
+		$('#results').append('<img src="https://myaccount.wigan.gov.uk/web/Includes/Images/loader.gif" height="200">'+'</image>');
 		$('#results').text(" ");
 		var query = $('#search_query').val();
 		$.get('https://api.spotify.com/v1/search?q='+query+'&type=track', function(data){
 			var results = data.tracks.items;
 			console.log(results);
 			var counter=0;
-			results.forEach(function(r){
-				var hrefId = '#' + counter;
-				$('#results').append('<a id="'+counter +'"href="">'+"<li>" + r.artists[0].name + ": " + r.name + "</a>" +" (popularity: " + r.popularity + ") - " + "<i>preview</i>"+ "</li>");
-				$(hrefId).attr("href",r.external_urls.spotify);
-				counter++;
-			});
-
+			if(results.length > 0){
+				results.forEach(function(r){
+					var albumImage = r.album.images[0].url;
+					$('#results').append('<img src="'+albumImage+'" height="100">'+'</image>'+'<br>');
+					$('#results').append('<a href="' + r.external_urls.spotify + '">' + "<b>" + "Song: " + r.name + "</b>" + "</a>");
+					$('#results').append("<br>" + "Artist: " + r.artists[0].name);
+					$('#results').append("<br>" + "Popularity: " + r.popularity + "<br>");
+					$('#results').append('<audio height="100" width="400" controls name="media">' + '<source src=' + r.preview_url + 'type="audio/mpeg">' + '</audio>'+'<br><br>');
+				});
+			} else {
+				$('#results').append("No Results Available");
+			}
 		});
 
 	});
