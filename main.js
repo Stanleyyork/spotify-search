90ee0dc0616e19ecb7494e57db722c75739da563
stanleyyork <stanleyyork@gmail.com>
2015-11-03 19:02:39 -0800

    added link to spotify url

--- main.js
+++ main.js
@@ -11,8 +11,12 @@ $(function() {
 		$.get('https://api.spotify.com/v1/search?q='+query+'&type=track', function(data){
 			var results = data.tracks.items;
 			console.log(results);
+			var counter=0;
 			results.forEach(function(r){
-				$('#results').append("<li>" + r.artists[0].name + ": " + r.name + "</li>");
+				var hrefId = '#' + counter;
+				$('#results').append('<a id="'+counter +'"href="">'+"<li>" + r.artists[0].name + ": " + r.name + "</li>"+"</a>");
+				$(hrefId).attr("href",r.external_urls.spotify);
+				counter++;
 			});
 
 		});
